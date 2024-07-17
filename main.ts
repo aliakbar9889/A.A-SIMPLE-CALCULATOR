367a5aed166830b2483a7f8b55f2ec320511ab8b
aliakbar9889 <themraliakbar@gmail.com>
2024-07-17 17:19:39 +0500

    A.A SIMPLE CALCULATOR

---
+++ main.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+console.log("WELCOME TO A.A SIMPLE CALCULATOR!!");
+let answer=await inquirer.prompt([
+    {
+        name:"firstNumber",
+        type:"number",
+        message:"ENTER YOUR FIRST NUMBER"
+        
+
+    },
+    {
+        name:"secondNumber",
+        type:"number",
+        message:"ENTER YOUR SECOND NUMBER"
+        
+    },
+    {
+        name:"operation",
+        type:"list",
+        message:"SELECT ONE OPERATION TO PERFORM",
+        choices:["ADDITION","SUBSTRACTION","MULTIPLICATION","DIVISION"]
+        
+    }
+
+
+]);
+if(answer.operation==="ADDITION"){
+    console.log(answer.firstNumber + answer.secondNumber);
+    }
+    else if(answer.operation === "SUBSTRACTION"){
+        console.log(answer.firstNumber - answer.secondNumber);
+}
+else if(answer.operation==="MULTIPLICATION"){
+    console.log(answer.firstNumber * answer.secondNumber);
+    
+
+}
+else if(answer.operation==="DIVISION"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else{
+    console.log("INVALID INPUT");
+    
+}
+console.log("THANKS FOR USING A.A SIMPLE CALCULATOR");
+
+
